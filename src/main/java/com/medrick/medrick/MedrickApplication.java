3b17e0eae44bc540b6943a93934c9936075495a2
s.b <manred1987@gmail.com>
2021-01-10 19:01:46 +0330

    Initial commit

---
+++ src/main/java/com/medrick/medrick/MedrickApplication.java
@@ -0,0 +1,13 @@
+package com.medrick.medrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedrickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedrickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medrick/medrick/MedrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medrick.medrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedrickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
